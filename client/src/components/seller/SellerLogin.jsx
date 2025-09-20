9dada746e80203ea2803c91cebe09810ddb26154
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-20 12:20:37 +0530

    seller layout  section  is  added

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -23,13 +23,13 @@ const SellerLogin = () => {
         <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span>Login</p>
         <div className='w-full'>
            <p>Email</p>
-           <input type="email" placeholder='enter your email' className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary' required/>
+           <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='enter your email' className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary' required/>
         </div>
         <div className='w-full'>
            <p>Password</p>
-           <input type="password" placeholder='enter your password' className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary' required/>
+           <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder='enter your password' className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary' required/>
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md cursor-progress'>Login</button>
+        <button className='bg-primary text-white w-full py-2 rounded-md cursor-pointer'>Login</button>
       </div>
     </form>
   )
