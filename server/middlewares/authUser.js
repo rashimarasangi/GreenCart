00aa6f1a5ccc7abdf0de652bfc0fcff10122dd5c
rashimarasangi <rashimarasangi200@gmail.com>
2025-10-04 21:08:30 +0530

    authUser is added

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized" });
+  }
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+    if (tokenDecode.id) {
+      req.body.userId = tokenDecode.id;
+    } else {
+      return res.json({ sucess: false, message: "Not Authorized" });
+    }
+    next();
+  } catch (error) {
+    res.json({ sucess: false, message: "Not Authorized" });
+  }
+};
+export default authUser;
