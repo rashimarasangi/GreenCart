02f9805619724a4d51c8cfc32e9cb8e23a4dbfb2
rashimarasangi <rashimarasangi200@gmail.com>
2025-10-22 20:55:25 +0530

    updated sections are added

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,4 +1,4 @@
-import User from "../models/user";
+import User from "../models/user.js";
 
 // update user cart data : /api/cart/update
 export const updateCart = async (req, res) => {
@@ -6,5 +6,8 @@ export const updateCart = async (req, res) => {
     const { userId, cartItems } = req.body;
     await User.findByIdAndUpdate(userId, { cartItems });
     res.json({ success: true, message: "Cart Updated" });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
